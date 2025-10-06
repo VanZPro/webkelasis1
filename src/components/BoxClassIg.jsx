496bb13a9492d37c109915639aa7ddefd4d569eb
VanZPro <evanluis198@gmail.com>
2025-10-06 19:01:33 +0700

    update: fix layout

--- src/components/BoxClassIg.jsx
+++ src/components/BoxClassIg.jsx
@@ -6,10 +6,9 @@ const BoxClassIg = () => {
 					<img src="/Instagram.svg" alt="" className="w-auto h-10" />
 					<img src="/next.png" alt="" className="h-4 w-4" />
 				</div>
-				<h1 className="text-white text-lg font-semibold pr-3 mt-3 absolute bottom-10">
+				<h1 className="text-white text-lg font-semibold pr-3 mt-3 absolute">
 				Instagram
 				</h1>
-				<div className="text-white flex py-2 opacity-60 absolute bottom-1 text-xs">View More</div>
 			</a>
 		</div>
 	)
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '192.168.1.35', // biar bisa diakses lewat IP lokal
+    port: 5173,      // port default Vite, bisa diganti misalnya 3000
+  },
 })
